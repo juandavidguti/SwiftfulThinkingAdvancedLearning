024643bebda5cc6ca011eaa1b89f862a8f224b40
Juan David <juandaguti@icloud.com>
2025-10-23 19:48:16 +0100

    Initial Commit

---
+++ SwiftfulAdvancedLearning/SwiftfulAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulAdvancedLearningApp.swift
+//  SwiftfulAdvancedLearning
+//
+//  Created by Juan David Gutierrez Olarte on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
