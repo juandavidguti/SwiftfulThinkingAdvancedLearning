e4e74469a73fffc2b4e913848ec32946fc6369ad
Juan David Gutierrez <juan@airapps.com>
2025-10-29 09:24:48 +0000

    Initial Commit

---
+++ SwiftfulThinkingAdvancedLearningTests/SwiftfulThinkingAdvancedLearningTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftfulThinkingAdvancedLearningTests.swift
+//  SwiftfulThinkingAdvancedLearningTests
+//
+//  Created by JUAN OLARTE on 10/29/25.
+//
+
+import Testing
+
+struct SwiftfulThinkingAdvancedLearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
